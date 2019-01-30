378636ac30c0f809f82efe736a5018c67a4046c3
Sid <netrobo@gmail.com>
2019-01-30 22:59:59 +0800

    Add QueryRow() to DB interface

--- types.go
+++ types.go
@@ -5,6 +5,7 @@ import "database/sql"
 // DB is a interface of *sql.DB and *sql.Tx.
 type DB interface {
 	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
 	Exec(query string, args ...interface{}) (sql.Result, error)
 }
 
