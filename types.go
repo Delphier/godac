d72b295ea0c71e087ef5b595596ac928be35be0c
Sid <netrobo@gmail.com>
2020-04-15 21:08:03 +0800

    Add refresh param to Result.Record(): set refresh is true to requery from database

--- types.go
+++ types.go
@@ -14,7 +14,7 @@ type Map map[string]interface{}
 
 // Result is an extension of sql.Result.
 type Result interface {
-	Record() (Map, error) // Get last Insert or Update record from database.
+	Record(refresh bool) (Map, error) // Get last Insert/Update record, set refresh is true to requery from database.
 	sql.Result
 }
 
@@ -23,18 +23,27 @@ type result struct {
 	isInsert  bool
 	db        DB
 	table     *Table
-	pk        Map
+	record    Map
 }
 
-func (r result) Record() (Map, error) {
+func (r result) Record(refresh bool) (Map, error) {
+	if !refresh {
+		return r.record, nil
+	}
+	var record = Map{}
 	if r.isInsert && r.table.autoInc >= 0 && r.table.Fields[r.table.autoInc].PrimaryKey {
 		id, err := r.sqlResult.LastInsertId()
 		if err != nil {
 			return nil, err
 		}
-		r.pk[r.table.keys[r.table.autoInc]] = id
+		for key, value := range r.record {
+			record[key] = value
+		}
+		record[r.table.keys[r.table.autoInc]] = id
+	} else {
+		record = r.record
 	}
-	query, args, err := r.table.WherePrimaryKey(r.pk)
+	query, args, err := r.table.WherePrimaryKey(record)
 	if err != nil {
 		return nil, err
 	}
