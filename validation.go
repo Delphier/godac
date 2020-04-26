97261ae9522e07f7fa129cede696a7382529e19d
Sid <netrobo@gmail.com>
2020-04-26 21:13:28 +0800

    Add In validation rule

--- validation.go
+++ validation.go
@@ -6,17 +6,20 @@ import (
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 )
 
-// Unique is a validation rule that checks if a value is unique in table.
-var Unique = &uniqueRule{}
-
-// ErrUnique is validation error.
-var ErrUnique = validation.NewError("unique", "already exists")
+// Validation errors.
+var (
+	ErrUnique = validation.NewError("unique", "already exists")
+	ErrIn     = validation.NewError("in", "not exists")
+)
 
 // ValidationRule is an extension of validation.Rule
 type ValidationRule interface {
 	SetContext(Context)
 	validation.Rule
 }
+
+// Unique is a validation rule that checks if a value is unique in table.
+var Unique = &uniqueRule{}
 
 type uniqueRule struct {
 	context Context
@@ -38,6 +41,34 @@ func (rule *uniqueRule) Validate(value interface{}) (err error) {
 	}
 	if count > 0 {
 		return ErrUnique
+	}
+	return nil
+}
+
+// In return a validation rule that check field value is in table.
+func In(table *Table) *InRule {
+	return &InRule{table: table}
+}
+
+// InRule is in validation rule object.
+type InRule struct {
+	context Context
+	table   *Table
+}
+
+// SetContext implements ValidationRule.
+func (rule *InRule) SetContext(c Context) {
+	rule.context = c
+}
+
+// Validate implements validation.Rule.
+func (rule *InRule) Validate(value interface{}) (err error) {
+	count, err := rule.table.CountRecord(rule.context.DB, rule.context.Field, rule.context.Record, false, sqlbuilder.Select())
+	if err != nil {
+		return err
+	}
+	if count <= 0 {
+		return ErrIn
 	}
 	return nil
 }
